1062a0d25db7efb0cf64b4465d1947de33412d64
esha <eshapabari8@gmail.com>
2020-11-21 19:48:24 +0530

    done1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,7 @@
 public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("hello world");
-		System.out.println("How are you");
-	System.out.println(1/0);
+	
 		
 	}
 }
